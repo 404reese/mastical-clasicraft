92f42a2b9ed30a502f64b153e97285c4fc6c0a88
Riddhesh <135740066+404reese@users.noreply.github.com>
2025-05-12 20:34:43 +0530

    init push

---
+++ utils/products.ts
@@ -0,0 +1,41 @@
+export function getProducts() {
+  return [
+    {
+      id: 1,
+      name: 'Stackable Chair',
+      price: 87.00,
+      imageUrl: '/chair.png',
+    },
+    {
+      id: 2,
+      name: 'Lamp Tool',
+      price: 35.00,
+      imageUrl: '/chair.png',
+    },
+    {
+      id: 3,
+      name: 'Arla Accent Chair',
+      price: 55.00,
+      imageUrl: '/chair.png',
+    },
+    {
+      id: 4,
+      name: 'Wooden Table',
+      price: 120.00,
+      imageUrl: '/chair.png',
+    },
+    {
+      id: 5,
+      name: 'Wooden Tray',
+      price: 25.00,
+      imageUrl: '/chair.png',
+    },
+    {
+      id: 6,
+      name: 'Magazine Rack',
+      price: 75.00,
+      imageUrl: '/chair.png',
+    },
+  ];
+}
+
